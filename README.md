499a4e13637cff1bce40b362a0be4e750c78b1d4
Ezrasanga <ezrasanga013@gmail.com>
2024-11-07 00:30:48 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # week1_python
+Objective: To learn how to take user input in Python
+
+
+
+
+Write a program that asks the user for their name, age, and location and then prints out a personalized message.
---
+++ index.py
@@ -0,0 +1,7 @@
+# Ask the user for their name, age, and location
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Print out a personalized message
+print(f"Hello, {name}! You are {age} years old and live in {location}. Nice to meet you!")
